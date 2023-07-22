08cb38adc2fb210f7fefb53d54acc1dc3eb2a081
Culf-B <49363745+Culf-B@users.noreply.github.com>
2023-07-22 14:39:14 +0200

    Command cooldown system

--- index.js
+++ index.js
@@ -3,28 +3,57 @@ const { Client, Collection, Intents } = require('discord.js');
 const { token } = require('./config.json');
 const Manager = require('./classes/manager.js');
 
+// Init client
 const client = new Client({ intents: [] });
 const manager = new Manager.Manager();
 
+// Load commands
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.data.name, command);
 }
+
+client.cooldowns = new Collection();
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('interactionCreate', async interaction => {
+	// Validate input
 	if (!interaction.isCommand()) return;
 
 	const command = client.commands.get(interaction.commandName);
 
 	if (!command) return;
+	
+	// Cooldown
+	const { cooldowns } = client;
 
+	if (!cooldowns.has(command.data.name)) {
+		cooldowns.set(command.data.name, new Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.data.name);
+	const defaultCooldownDuration = 3;
+	const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1000;
+
+	if (timestamps.has(interaction.user.id)) {
+		const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+		if (now < expirationTime) {
+			const expiredTimestamp = Math.round(expirationTime / 1000);
+			return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+		}
+	}
+
+	timestamps.set(interaction.user.id, now);
+	setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+	// Try catch for executing command
 	try {
 		await command.execute(interaction, manager);
 	} catch (error) {
