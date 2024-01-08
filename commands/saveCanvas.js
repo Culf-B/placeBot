4916a425eac9f0bfe4f607c1ff5f84966b4a5abc
Culf-B <49363745+Culf-B@users.noreply.github.com>
2024-01-08 23:09:19 +0100

    Save canvas function can now only be used by me

--- commands/saveCanvas.js
+++ commands/saveCanvas.js
@@ -3,10 +3,14 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('save_canvas')
-		.setDescription('Save the canvas')
+		.setDescription('Save the canvas (This command can only be used by user culf who created this bot)')
 		.setDefaultMemberPermissions(0),
 	async execute(interaction, manager) {
-        manager.canvasManager.save();
-		interaction.reply({ content: "Canvas saved!", ephemeral: true });
+		if (interaction.user.id == '506113391665086484') {
+			manager.canvasManager.save();
+			interaction.reply({ content: "Canvas saved!", ephemeral: true });
+		} else {
+			interaction.reply({ content: "Only my creator has permission to use that command!", ephemeral: true });
+		}
 	},
 };
